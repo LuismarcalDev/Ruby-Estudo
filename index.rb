3b6f96c9598ed210ef720ae2052b5eb5841c958b
luis <luisfelippe55482@gmail.com>
2025-08-22 16:44:25 -0300

    projeto feito

--- index.rb
+++ index.rb
@@ -1,28 +1,27 @@
+
+
 class Produto
-    def initialize(nome,descricao,valor)
+    def initialize(nome,marca,valor)
         @nome = nome
-        @descricao = descricao
+        @marca = marca
         @valor = valor
     end
 
     def imprimir
-        puts "Nome do produto: #{@nome}\n Descrição: #{@descricao}\n Valor: R$#{@valor}" 
+        puts "nemo: #{@nome} marca: #{@marca} valor: R$#{@valor}"
     end
 end
 
-p1 = Produto.new("Galxy m53","smartphone 8gb Ram", 1200)
-p2 = Produto.new("Iphone 15"," 12gb Ram", 5200)
-p3 = Produto.new("Nexus pro 2","smartphone", 2800)
-p1.imprimir
-p2.imprimir
-p3.imprimir
 
 
-class Produto
-    def initialize(nome, descricao,valor)
-        @nome
-        @descricao
-        @valor
-    end
-
+puts "qual o nome do produto?"
+    nomeU = gets.chomp
     
+    puts "qual o nome da marca?"
+    marcaU = gets.chomp
+    
+    puts "qual o valor do produto?"
+    valorU = gets.chomp
+
+    p1 = Produto.new(nomeU,marcaU,valorU)
+    p1.imprimir
